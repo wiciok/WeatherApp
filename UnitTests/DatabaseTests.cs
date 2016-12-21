fb04eb2678813a3505dc88527a5c5286c27f8671
Qhoros <he.lukasz@gmail.com>
2016-12-21 20:56:32 +0100

    Database controller works properly

---
+++ UnitTests/DatabaseTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using WeatherApp.Database;
+
+namespace WeatherApp.Tests
+{
+    [TestFixture]
+    class UnitTests
+    {
+        [Test]
+        public static void CheckDatabaseSelectMethod()
+        {
+            Assert.IsNull(new WeatherDatabaseController().Select("to nie jest poprawne zapytanie"));
+            Assert.IsNotNull(new WeatherDatabaseController().Select("SELECT * FROM TEMPERATURES"));
+        }
+
+        [Test]
+        public static void CheckDatabaseCountMethod()
+        {
+            Assert.AreEqual(-1, new WeatherDatabaseController().Count("to jest niepoprawne zapytanie"));
+            Assert.AreEqual(0, new WeatherDatabaseController().Count("SELECT COUNT(*) FROM TEMPERATURES"));
+            Assert.AreEqual(1, new WeatherDatabaseController().Count("SELECT COUNT(*) FROM UNITS"));
+        }
+    }
+}
