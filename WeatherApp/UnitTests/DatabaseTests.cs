abb5e7a68746f3152ea1d5672a0d456ebeb1a33b
Qhoros <he.lukasz@gmail.com>
2016-12-22 12:13:19 +0100

    Databse connection established

---
+++ WeatherApp/UnitTests/DatabaseTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using WeatherApp.Database;
+
+namespace WeatherApp.Tests
+{
+    [TestFixture]
+    class UnitTests
+    {
+        [Test]
+        public static void CheckDatabaseSelectMethod()
+        {
+            Assert.IsNull(new WeatherDatabaseController().Select("to nie jest poprawne zapytanie"));
+            Assert.IsNotNull(new WeatherDatabaseController().Select("SELECT * FROM TEMPERATURES"));
+        }
+
+        [Test]
+        public static void CheckDatabaseCountMethod()
+        {
+            Assert.AreEqual(-1, new WeatherDatabaseController().Count("to jest niepoprawne zapytanie"));
+            Assert.AreEqual(4, new WeatherDatabaseController().Count("SELECT COUNT(*) FROM UNITS"));
+        }
+    }
+}
