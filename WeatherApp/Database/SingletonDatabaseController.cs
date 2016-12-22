cbace9da76189ca4cbb73738628db1c8a792853d
Qhoros <he.lukasz@gmail.com>
2016-12-22 12:22:14 +0100

    Database controller singleton added

---
+++ WeatherApp/Database/SingletonDatabaseController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Database
+{
+    class SingletonDatabaseController
+    {
+        private static SingletonDatabaseController instance;
+        private DatabaseController dbController = new WeatherDatabaseController();
+
+        private SingletonDatabaseController() { }
+
+        public static SingletonDatabaseController Instance
+        {
+            get 
+            {
+                if (instance == null)
+                    instance = new SingletonDatabaseController();
+
+                return instance;
+            }
+        }
+
+        public DatabaseController DbController
+        {
+            get
+            {
+                return dbController;
+            }
+        }
+
+    }
+}
--- WeatherApp/Program.cs
+++ WeatherApp/Program.cs
@@ -11,16 +11,7 @@ namespace WeatherApp
     {
         static void Main(string[] args)
         {
-            DatabaseController dbController = new WeatherDatabaseController();
-
-            dbController.Insert("INSERT INTO TEMPERATURES (TEMPERATURE_VALUE, UNIT_ID) VALUES (1, 4);");
-
-            foreach (List<string> list in dbController.Select("SELECT * FROM TEMPERATURES;"))
-            {
-                Console.WriteLine(list[0]);
-            }
-
-            dbController.Delete("DELETE FROM TEMPERATURES WHERE TEMPERATURE_VALUE=1");
+            SingletonDatabaseController.Instance.DbController.Insert("INSERT INTO TEMPERATURES (TEMPERATURE_VALUE, UNIT_ID) VALUES (1, 4);");
 
             Console.ReadLine();
         }
